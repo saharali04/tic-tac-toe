bb1aefffa2cd9baaf274b51cef9827cc4cdfbfed
Sahar Ali <saharnazimali@gmail.com>
2023-10-05 13:36:21 -0400

    Adds Tic-Tac-Toe board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({ value }) {
+  return <button className='square'>{value}</button>;
+}
+export default function Board() {
+  return (
+    <Fragment>
+      <div className="board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      <div className="board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+      <div className="board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
+    </Fragment>
+  );
 }
